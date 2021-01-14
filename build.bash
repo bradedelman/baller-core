6e778a2d1cea7586c5ab8d02327bffda7e82f35d
Brad Edelman <brad.edelman@affirm.com>
2021-01-13 16:04:05 -0800

    smarter build script that only runs tsc if the ts is newer than the js

--- build.bash
+++ build.bash
@@ -1,1 +1,3 @@
-tsc --target es5 --module es2015 --moduleResolution node  --lib dom,es2016,es5 --module amd --out $1.js $1.ts
+if [[ $1.ts -nt $1.js ]]; then
+    tsc --target es5 --module es2015 --moduleResolution node  --lib dom,es2016,es5 --module amd --out $1.js $1.ts
+fi
